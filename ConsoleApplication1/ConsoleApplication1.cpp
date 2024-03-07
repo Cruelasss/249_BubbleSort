dd477499995f5c413f8200aec42e18a547374c44
Cruelas <irula5198@gmail.com>
2024-03-07 19:29:26 +0700

    membuat prosedur

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,3 +5,24 @@ using namespace std;
 int arr[20];
 int n;
 
+void input() {
+    while (true) {
+        cout << "masukkan banyaknya elemen pada array :";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\n Array dapat mempunyai maksimal 20 elemen./n";
+        }
+        cout << endl;
+        cout << "===============" << endl;
+        cout << "Masukkan elemen array" << endl;
+        cout << "===============" << endl;
+
+        for (int i = 0; i < n; i++) {
+            cout << "data ke-" << (i + 1) << ":";
+            cin >> arr[i];
+        }
+    }
+}
+
