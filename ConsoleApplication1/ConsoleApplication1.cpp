6008f4fff0f83ed9210074596fffa6b0910a62d1
Cruelas <irula5198@gmail.com>
2024-03-07 19:30:56 +0700

    membuat agar menampilkan element array

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -45,6 +45,17 @@ void bubbleSortArray() {//procedur untuk mengurutkan array dengan metode bubble 
     } while (pass <= n);//step 5
 
 }
+void display() {
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Element array yang telah tersusun " << endl;
+    cout << "======================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl; // Output setiap element array pada garis baru 
+    }
+    cout << "Jumlah pass = " << n - 1 << endl;// menampilkan total angka dari elemen yang benar 
+    cout << endl;
+}
 
 
 
